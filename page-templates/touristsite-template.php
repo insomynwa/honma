30cafeabe7c1ebbc3cf618792a0fe219b2f9f80b
Pika <pikamons@gmail.com>
2016-06-26 23:44:15 +0700

    General Update
    Tourist List, Map
    
    Next: detail tourist

--- page-templates/touristsite-template.php
+++ page-templates/touristsite-template.php
@@ -44,10 +44,10 @@ get_header( 'sltg' );
 								<div class="content-holder">
 									<div class="content-holder-body">
 										<div class="chb-column column-10">
-											<ul class="mini-opt-left0">
-												<li class="active"><a id="0thumbnail-model-1" class="0thumbnail-model-link" href="#">Grid</a></li>
-												<li><a id="0thumbnail-model-2" class="0thumbnail-model-link" href="#">List</a></li>
-												<li><a id="0thumbnail-model-3" class="0thumbnail-model-link" href="#">Map</a></li>
+											<ul class="thumb-mode-body mini-opt-left0">
+												<li class="active"><a id="thumb-mode-1 0thumbnail-model-1" class="thumb-mode 0thumbnail-model-link" href="#">Grid</a></li>
+												<li><a id="thumb-mode-2 0thumbnail-model-2" class="thumb-mode 0thumbnail-model-link" href="#">List</a></li>
+												<li><a id="thumb-mode-3 0thumbnail-model-3" class="thumb-mode 0thumbnail-model-link" href="#">Map</a></li>
 											</ul>
 										</div>
 									</div>
@@ -60,39 +60,39 @@ get_header( 'sltg' );
 											<?php foreach($arrKats as $kat) : ?>
 											<!-- <div class="ctn-md-large-max0"> -->
 												<!-- <div ><h2><?php // _e( strtoupper($kat->GetNama()) ); ?></h2></div> -->
-											<div class="chb-column column-10 grid-body grid-list-area0">
-												<?php foreach( $kat->GetTouristSites() as $ts ): ?>
-														<div class="grid-column g-column-4 thumbnail-model0 thumbnail-grid0"> <!-- thumbnail-grid && thumbnail-grid-img -->
-															<div class="grid-content">
-																<a class="img-a link-img0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
-																<div class="thumbnail-model-img0 thumbnail-grid-img0" style="background-image:url(<?php _e( $ts->GetGambarUtama()->GetLinkGambar() ); ?>?<?php echo millitime(); ?>)"></div>
-																</a>
-																<a class="title-a link-title0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
-																<h4><?php _e( $ts->GetNama() ); ?></h4>
-																</a>
-																<p class="thumbnail-model-desc0 thumbnail-grid-description0"><?php _e( explode('. ', $ts->GetDeskripsi())[0] ); ?></p>
+												<div class="chb-column column-10 thumb-holder grid-list-area0">
+													<?php foreach( $kat->GetTouristSites() as $ts ): ?>
+															<div class="thumb-holder-body thb-column-4 thumbnail-model0 thumbnail-grid0"> <!-- tbh-column-4 thumbnail-grid && thumbnail-grid-img -->
+																<div class="thb-content grid-content">  <!-- grid-content -->
+																	<a class="img-a link-img0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
+																	<div class="thumbnail-model-img0 thumbnail-grid-img0" style="background-image:url(<?php _e( $ts->GetGambarUtama()->GetLinkGambar() ); ?>?<?php echo millitime(); ?>)"></div>
+																	</a>
+																	<a class="title-a link-title0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
+																	<h4><?php _e( $ts->GetNama() ); ?></h4>
+																	</a>
+																	<p class="thumbnail-model-desc0 thumbnail-grid-description0"><?php _e( explode('. ', $ts->GetDeskripsi())[0] ); ?></p>
+																</div>
 															</div>
-														</div>
-												<?php endforeach; ?>
-												<?php foreach( $kat->GetTouristSites() as $ts ): ?>
-														<div class="grid-column g-column-4 thumbnail-model0 thumbnail-grid0"> <!-- thumbnail-grid && thumbnail-grid-img -->
-															<div class="grid-content">
-																<a class="img-a link-img0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
-																<div class="thumbnail-model-img0 thumbnail-grid-img0" style="background-image:url(<?php _e( $ts->GetGambarUtama()->GetLinkGambar() ); ?>?<?php echo millitime(); ?>)"></div>
-																</a>
-																<a class="title-a link-title0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
-																<h4><?php _e( $ts->GetNama() ); ?></h4>
-																</a>
-																<p class="thumbnail-model-desc0 thumbnail-grid-description0"><?php _e( explode('. ', $ts->GetDeskripsi())[0] ); ?></p>
+													<?php endforeach; ?>
+													<?php foreach( $kat->GetTouristSites() as $ts ): ?>
+															<div class="thumb-holder-body thb-column-4 thumbnail-model0 thumbnail-grid0"> <!-- thumbnail-grid && thumbnail-grid-img -->
+																<div class="thb-content grid-content">
+																	<a class="img-a link-img0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
+																	<div class="thumbnail-model-img0 thumbnail-grid-img0" style="background-image:url(<?php _e( $ts->GetGambarUtama()->GetLinkGambar() ); ?>?<?php echo millitime(); ?>)"></div>
+																	</a>
+																	<a class="title-a link-title0" href="<?php echo home_url().'/detail-tourist-site?touristsite='. $ts->GetID(); ?>">
+																	<h4><?php _e( $ts->GetNama() ); ?></h4>
+																	</a>
+																	<p class="thumbnail-model-desc0 thumbnail-grid-description0"><?php _e( explode('. ', $ts->GetDeskripsi())[0] ); ?></p>
+																</div>
 															</div>
-														</div>
-												<?php endforeach; ?>
-											</div>
-												<div id="sltg-map"></div>
+													<?php endforeach; ?>
+												</div>
 											<!-- </div> -->
 											<?php endforeach; ?>
 										<?php endif; ?>
 									</div>
+									<div id="sltg-map" class=""></div>
 								</div>
 							</div>
 						</div> <!-- END wrap-body -->
@@ -113,11 +113,19 @@ jQuery(document).ready( function($) {
 	$( ".grid-content").hover( function(){
 		$( this ).children('a.title-a').children('h4').addClass('to-top');
 		$( this ).children('a.img-a').children('div').addClass('transparent-down');
-		$( this ).children('p').fadeIn();
+		$( this ).children('p').css('display','block');
 	}, function() {
 		$( this ).children('a.title-a').children('h4').removeClass('to-top');
 		$( this ).children('a.img-a').children('div').removeClass('transparent-down');
-		$( this ).children('p').fadeOut();
+		if( $(this).hasClass( 'list-content') == false ) {
+			$( this ).children('p').css('display','none');
+		}
+	});
+
+	$( ".list-content .img-a div, .list-content .title-a h4").hover( function(){
+		$( this ).parent().parent().children('a.img-a').children('div').addClass('transparent-down');
+	}, function() {
+		$( this ).parent().parent().children('a.img-a').children('div').removeClass('transparent-down');
 	});
 
 	/*$( ".thumbnail-grid p").hover( function(){
@@ -126,32 +134,38 @@ jQuery(document).ready( function($) {
 		$( ".thumbnail-grid p").hide();
 	});*/
 
-	$( "a.thumbnail-model-link").click( function(){
+	$( "a.thumb-mode").click( function(){
 		//alert(this.id);
-		$( ".mini-opt-left li").removeClass( "active" );
+		$( ".thumb-mode-body li").removeClass( "active" );
 		$(this).parent().addClass('active');
 
 		var type = (this.id).split('-').pop();
 		if( type==1){
-			$( ".thumbnail-model").removeClass('thumbnail-list');
-			$( ".thumbnail-model").addClass('thumbnail-grid');
-			$( ".thumbnail-model-img").removeClass('thumbnail-list-img');
-			$( ".thumbnail-model-img").addClass('thumbnail-grid-img');
-			$( ".thumbnail-model-desc").removeClass('thumbnail-list-description');
-			$( ".thumbnail-model-desc").addClass('thumbnail-grid-description');
+			$( ".thb-content").children('p').css('display','none');
+			$( ".thumb-holder-body").removeClass('thb-column-2');
+			$( ".thumb-holder-body").addClass('thb-column-4');
+			$( ".thb-content").removeClass('list-content');
+			$( ".thb-content").addClass( 'grid-content' );
+			// $( ".thumbnail-model-img").removeClass('thumbnail-list-img');
+			// $( ".thumbnail-model-img").addClass('thumbnail-grid-img');
+			// $( ".thumbnail-model-desc").removeClass('thumbnail-list-description');
+			// $( ".thumbnail-model-desc").addClass('thumbnail-grid-description');
 			$( "#sltg-map").fadeOut('fast');
-			$( ".thumbnail-model").fadeIn();
+			$( ".thumb-holder").fadeIn();
 		}else if( type==2){
-			$( ".thumbnail-model").removeClass('thumbnail-grid');
-			$( ".thumbnail-model").addClass('thumbnail-list');
-			$( ".thumbnail-model-img").removeClass('thumbnail-grid-img');
-			$( ".thumbnail-model-img").addClass('thumbnail-list-img');
-			$( ".thumbnail-model-desc").addClass('thumbnail-list-description');
-			$( ".thumbnail-model-desc").removeClass('thumbnail-grid-description');
+			$( ".thb-content").children('p').css('display','block');
+			$( ".thumb-holder-body").removeClass('thb-column-4');
+			$( ".thumb-holder-body").addClass('thb-column-2');
+			$( ".thb-content").removeClass('grid-content');
+			$( ".thb-content").addClass( 'list-content' );
+			// $( ".thumbnail-model-img").removeClass('thumbnail-grid-img');
+			// $( ".thumbnail-model-img").addClass('thumbnail-list-img');
+			// $( ".thumbnail-model-desc").addClass('thumbnail-list-description');
+			// $( ".thumbnail-model-desc").removeClass('thumbnail-grid-description');
 			$( "#sltg-map").fadeOut('fast');
-			$( ".thumbnail-model").fadeIn();
+			$( ".thumb-holder").fadeIn();
 		}else if( type==3) {
-			$( ".thumbnail-model").hide();
+			$( ".thumb-holder").hide();
 			$( "#sltg-map").fadeIn();
 		}
 	});
