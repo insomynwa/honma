c212c486fa1b16ae32b949b889c9290ef4ebeeb0
Pika <pikamons@gmail.com>
2016-06-29 22:52:53 +0700

    General Update
    Music Template
    
    Next: Hotel

--- templates/music-list-template.php
+++ templates/music-list-template.php
@@ -2,15 +2,16 @@
 	$data = $attributes[ 'music' ];
 ?>
 <?php if( sizeof( $data ) > 0 ): ?>
-	<div class="content-middle">
-		<ul>
+	<!-- <div class="content-middle"> -->
+		<!-- <ul> -->
 		<?php foreach( $data as $m ): ?>
-			<li>
-				<div>
-					<?php echo do_shortcode( '[soundcloud url="https://api.soundcloud.com/tracks/'. $m->GetSource() . '" params="auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&visual=false&sharing=false&download=false&liking=false&buying=false" width="100%" height="120" iframe="true" /]' ); ?>
+			<!-- <li> -->
+				<div class="music-list">
+					<h4><?php _e( $m->GetCreator()->GetNama()); ?> - <?php _e( $m->GetTitle()); ?></h4>
+					<?php echo do_shortcode( '[soundcloud url="https://api.soundcloud.com/tracks/'. $m->GetSource() . '" params="auto_play=false&hide_related=true&color=f11d1d&show_comments=false&show_user=true&show_reposts=false&visual=false&sharing=false&download=false&liking=false&buying=false" width="100%" height="120" iframe="true" /]' ); ?>
 				</div>
-			</li>
+			<!-- </li> -->
 		<?php endforeach; ?>
-		</ul>
-	</div>
+		<!-- </ul> -->
+	<!-- </div> -->
 <?php endif; ?>
